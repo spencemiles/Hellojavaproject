2cb702cb4f82f0c303c3edcd69daabe88ea7582f
spencemiles <160491999+spencemiles@users.noreply.github.com>
2024-02-21 16:52:29 +0300

    this is a new line added in intellij

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
     System.out.println("hello git and github!");
+    System.out.println("this is a new line added in intellij");
     }
 }
