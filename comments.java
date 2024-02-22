2229c00f81fb29ef4121d8b2cdf82201e0e57384
spencemiles <160491999+spencemiles@users.noreply.github.com>
2024-02-22 15:45:38 +0300

    this is a new line added in intellij

---
+++ comments.java
@@ -0,0 +1,23 @@
+public class comments {
+    public static void main(String[] args){
+        /*comments in java
+        *we have three types of comments in java
+        * 1. C++ style comments -> //
+        * 2. C style comments
+        * 3.speial javadoc comments
+         */
+
+        System.out.println("we use // for c++ comment style");
+
+        /* 2.C comment styles *
+         */
+        System.out.println("2. we use /**/ for c comment style");
+        /**
+         * 3. special javadoc comment
+         */
+
+
+
+        System.out.println("3. we use /** */ for special javadoc comment");
+    }
+}
